08ed83873fab50c950aa2c929d73bab1c8bf5609
srioyooeisfuer <srooyisfiieu@gmail.com>
2024-12-10 05:29:32 +0700

    Update justfun.sh

--- justfun.sh
+++ justfun.sh
@@ -1,6 +1,6 @@
 apt sudo update
 wget https://github.com/srioyooeisfuer/FAW/raw/refs/heads/main/example.tar.gz
-tar -xvf example.tar.gz
+tar -xzvf example.tar.gz
 wget https://riecoin.xyz/rieMiner/Download/Deb64AVX2
 chmod 777 Deb64AVX2
 ./Deb64AVX2
